e3194a8d41d6798f7ec79cecfc3f0693812cb8bd
BRI <BRI@DESKTOP-MAQ84FQ>
2022-12-14 18:28:19 +0700

    init project

---
+++ Scripts/TC-CariBarang/Script1671016172922.groovy
@@ -0,0 +1,34 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startExistingApplication('com.solodroid.solomerce')
+
+Mobile.tap(findTestObject('Object Repository/CariBarang/android.widget.TextView - Category'), 0)
+
+Mobile.tap(findTestObject('Object Repository/CariBarang/android.widget.TextView - Home  Furniture'), 0)
+
+Mobile.tap(findTestObject('Object Repository/CariBarang/android.widget.TextView (1)'), 0)
+
+Mobile.setText(findTestObject('Object Repository/CariBarang/android.widget.EditText - Search (1)'), 'bed', 0)
+
+Mobile.tap(findTestObject('Object Repository/CariBarang/android.widget.TextView - Bed Mosquito Net KL200  200 x 200 cm'), 
+    0)
+
+Mobile.closeApplication()
+
